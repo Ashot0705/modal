4cb40d5e242e7e261f4dcde6ea77b2cb61c634e3
Ashot Arutyunyan <jcl9595@gmail.com>
2017-01-16 17:33:58 -0500

    modal pop-up

---
+++ js/index.js
@@ -0,0 +1,9 @@
+// function modal-pop() {
+// 	setTimeout(function() }, 3000);
+// }
+
+
+setTimeout(function() {
+	var modal = document.getElementsByClassName("modal-pop")[0].style.display = "block"
+	// document.getElementsByTagName("body")[0].addClass("overlay")
+}, 3000)
